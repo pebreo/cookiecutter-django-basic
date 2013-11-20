85cac12da9615b7dadb8ebfafa34dfcfb65e44e8
Paul <pebre79@hotmail.com>
2013-11-20 10:24:56 -0600

    Update urls.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 #from .views import {{ cookiecutter.model_name }}List, {{ cookiecutter.model_name }}Create, {{ cookiecutter.model_name }}Detail, {{ cookiecutter.model_name }}Update, {{ cookiecutter.model_name }}Delete
-
+#from . import views
 
 urlpatterns = patterns(
     '',
+    #url(r'^$', views.index, name='index'),
     #url(r'^$', {{ cookiecutter.model_name }}List.as_view(), name='{{ cookiecutter.model_name|lower }}_list'),
     #url(r'^new/$', {{ cookiecutter.model_name }}Create.as_view(), name='{{ cookiecutter.model_name|lower }}_create'),
     #url(r'^(?P<pk>\d+)/$', {{ cookiecutter.model_name }}Detail.as_view(), name='{{ cookiecutter.model_name|lower }}_detail'),
