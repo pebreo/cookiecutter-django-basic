c7b8f658f0020ef30c3f402f38262e33f482feed
Paul <pebre79@hotmail.com>
2013-11-20 10:32:30 -0600

    Update settings.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.proj_name}}/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.proj_name}}/settings.py
@@ -108,9 +108,7 @@ ROOT_URLCONF = '{{ cookiecutter.proj_name }}.urls'
 WSGI_APPLICATION = '{{ cookiecutter.proj_name }}.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    normpath(join(DJANGO_ROOT, 'templates')),
 )
 
 INSTALLED_APPS = (
